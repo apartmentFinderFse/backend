a96816ad7b01303568a2c507204450d9302e614d
shreyaskapoor22 <shreyas.kapoor@colorado.edu>
2023-03-04 16:32:36 -0700

    feature/Shreyas - Creating gradle project

---
+++ src/main/java/application/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package application.repository;
+
+import application.model.Users;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+
+@Repository
+public interface UserRepository extends MongoRepository<Users, Long> {
+
+
+}
