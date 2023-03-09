0559c5225cd0b940fae082529ac3a75a4973296d
shreyaskapoor22 <shreyas.kapoor@colorado.edu>
2023-03-08 18:39:58 -0700

    feature/Shreyas - updating project

--- src/main/java/application/controller/UserController.java
+++ src/main/java/application/controller/UserController.java
@@ -1,12 +1,16 @@
 package application.controller;
 
-import application.model.Users;
+import application.model.User;
+import application.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import application.repository.UserRepository;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1")
@@ -15,22 +19,28 @@ public class UserController {
     private static final Logger LOGGER = LoggerFactory.getLogger(UserController.class);
     @Autowired
     private final UserRepository userRepository;
+    @Autowired
+    private UserService userService;
     public UserController(UserRepository userRepository) {
         this.userRepository = userRepository;
     }
 
 
     @PostMapping("register")
-    @ResponseStatus(HttpStatus.CREATED)
-    public Users registerUser( @RequestBody Users user) {
-        LOGGER.info("Entering");
-        user.setId(user.getUserName());
-        return userRepository.insert(user);
+    public ResponseEntity<User> registerUser(@RequestBody User user) {
+        LOGGER.info("Registering a user {}", user.getUserName());
+        return new ResponseEntity<>(userService.insertUser(user), HttpStatus.CREATED);
     }
 
     @GetMapping("/")
     public String index() {
         LOGGER.info("test line");
-        return "Greetings from Spring Boot!";
+        return "Greetings from User Controller!";
+    }
+
+    @GetMapping("login")
+    public ResponseEntity<Optional<User>> findUser(@RequestBody User user) {
+        LOGGER.info("Fetching details of user {}", user.getUserName());
+        return new ResponseEntity<>(userService.findUserByUserName(user), HttpStatus.OK);
     }
 }
